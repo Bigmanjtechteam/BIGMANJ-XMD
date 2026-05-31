b5a3893b964228204e1e315b84a5a49fe67d8251
BIGMANj <brightsonnjegite@gmail.com>
2026-06-01 02:52:42 +0300

    Update menu-automation.js

--- commands/menu-automation.js
+++ commands/menu-automation.js
@@ -20,10 +20,11 @@ const handler = async (sock, chatId, m) => {
     caption += `━━━━━━━━━━━━━━━━━━━━━━\n`;
     for (const cmd of commands) caption += `• ${cmd}\n`;
     caption += `\n🤖 Automate your WhatsApp experience.\n💡 Let the bot work for you.\n\n`;
-    caption += `🚀 BIGMANj BOT — Fast • Powerful • Reliable\n\n> bigmanj tech™`;
+    caption += `🚀 *BIGMANj MD* — Fast • Powerful • Reliable\n\n> bigmanj tech™`;
 
+    await sock.sendMessage(chatId, { react: { text: '⚡', key: m.key } });
     await sock.sendMessage(chatId, {
-        image: { url: 'https://o.uguu.se/yeOyXUvm.jpg' }, // Picha yako mpya
+        image: { url: 'https://o.uguu.se/yeOyXUvm.jpg' },
         caption: caption,
         mentions: [senderId]
     }, { quoted: m });

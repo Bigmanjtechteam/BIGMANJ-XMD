0f49a5699fe8bda9613069af518b32ddbc068e87
BIGMANj <brightsonnjegite@gmail.com>
2026-06-01 02:52:18 +0300

    Update menu-fun.js

--- commands/menu-fun.js
+++ commands/menu-fun.js
@@ -20,10 +20,11 @@ const handler = async (sock, chatId, m) => {
     caption += `━━━━━━━━━━━━━━━━━━━━━━\n`;
     for (const cmd of commands) caption += `• ${cmd}\n`;
     caption += `\n🎮 Fun commands for entertainment.\n💡 Enjoy chatting with friends.\n\n`;
-    caption += `🚀 BIGMANj BOT — Fast • Powerful • Reliable\n\n> bigmanj tech™`;
+    caption += `🚀 *BIGMANj MD* — Fast • Powerful • Reliable\n\n> bigmanj tech™`;
 
+    await sock.sendMessage(chatId, { react: { text: '🎮', key: m.key } });
     await sock.sendMessage(chatId, {
-        image: { url: 'https://d.uguu.se/uCjHKvHJ.jpg' }, // Picha yako mpya kwa FUN MENU
+        image: { url: 'https://d.uguu.se/uCjHKvHJ.jpg' },
         caption: caption,
         mentions: [senderId]
     }, { quoted: m });
